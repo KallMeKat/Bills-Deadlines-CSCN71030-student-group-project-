bd0eb5f205fb83b85cd796990822cdb57661c098
Aleks9633 <113472803+Aleks9633@users.noreply.github.com>
2023-03-22 21:29:37 -0400

    Random encounter header and c files

--- TeamDevSoftware/randEvent.c
+++ TeamDevSoftware/randEvent.c
@@ -6,13 +6,14 @@
 // Function to generate the random events number (1-6)
 int random_number() {
 	srand(time(NULL));
-	return (rand() % 6) + 1;
+	return (rand() % 3) + 1;
 }
 
 //Function to create a new event node
-struct Encounter* createEncounter(char* description) {
+struct Encounter* createEncounter(int id, char desc[]) {
 	struct Encounter* newEncounter = (struct Encounter*)malloc(sizeof(struct Encounter));
-	newEncounter->description = description;
+	newEncounter->id = id;
+	strcpy(newEncounter->description, desc);
 	newEncounter->next = NULL;
 	return newEncounter;
 }
@@ -21,10 +22,12 @@ struct Encounter* createEncounter(char* description) {
 struct Encounter* selectEncounter(struct Encounter* head) {
 	int eventNumber = random_number();
 	struct Encounter* current = head;
-	while (current != NULL && eventNumber > 1) {
+	while (current != NULL) {
+		if (current->id == eventNumber) {
+			return current;
+		}
 		current = current->next;
-		eventNumber--;
 	}
-	return current;
+	return NULL;
 }
 
--- TeamDevSoftware/randEvent.h
+++ TeamDevSoftware/randEvent.h
@@ -4,13 +4,14 @@
 
 //Struct for the linked list of random events
 struct Encounter {
-	char* description;
+	int id;
+	char* description[100];
 	struct Enounter* next;
 	//Add other values that come with the encounter struct that affect your player struct, such as your money, morale etc.
 };
 
 
-struct Encounter* createEncounter(char* description);
+struct Encounter* createEncounter(int id, char desc[]);
 struct Encounter* selectEncounter(struct Encounter* head);
 int random_number();
 
